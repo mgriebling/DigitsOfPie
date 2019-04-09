169e2b4720f04fa0ac4f50845d209e58a47b4c93
Michael Griebling <michael.griebling@solinst.com>
2019-04-09 07:32:56 -0400

    Initial Commit

---
+++ DigitsOfPie/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DigitsOfPie
+//
+//  Created by Michael Griebling on 4/9/19.
+//  Copyright © 2019 Computer Inspirations. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DigitsOfPie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DigitsOfPie
+//
+//  Created by Michael Griebling on 4/9/19.
+//  Copyright © 2019 Computer Inspirations. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
